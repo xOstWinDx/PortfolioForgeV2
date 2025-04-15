b05e96d50814901a51a8c1de1fbc972d8a6f3a7d
Alexey <Starobogatov.a@yandex.ru>
2025-04-15 06:49:03 +0300

    add logics by add images in workflow

--- auth/src/infrastructure/repo.py
+++ auth/src/infrastructure/repo.py
@@ -1,10 +1,10 @@
 from sqlalchemy import select, exists
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.application.interfaces.repo import IUserRepository
+from src.application.interfaces.repo import IUserRepository, IImageRepository
 from src.domain.filter import UserFilter
 from src.domain.user import User
-from src.infrastructure.models import UserModel, RoleModel
+from src.infrastructure.entities.models import RoleModel, UserModel, ImageModel
 
 
 class SQLUserRepository(IUserRepository):
@@ -22,10 +22,16 @@ class SQLUserRepository(IUserRepository):
             self.session.add(role)
             await self.session.flush()
 
+        avatar = await self.session.merge(
+            ImageModel(id=user.avatar.id, file_url=user.avatar.file_url)
+        )
+
         user_model = self.model.from_domain(user, role.id)
         self.session.add(user_model)
         await self.session.flush()
         user_model.role = role
+        user_model.avatar = avatar
+
         return user_model.to_domain()
 
     async def exists(self, filter: UserFilter) -> bool:
@@ -55,8 +61,10 @@ class SQLUserRepository(IUserRepository):
 
     async def update(self, user: User) -> User:
         role = await self._get_role_by_name(user.role.value)
+        image = await self._get_image_by_id(user.avatar.id)
         user_model = self.model.from_domain(user, role_id=role.id)
         user_model.role = role
+        user_model.avatar = image
         await self.session.merge(user_model)
         await self.session.flush()
         return user_model.to_domain()
@@ -66,3 +74,26 @@ class SQLUserRepository(IUserRepository):
         res = await self.session.execute(query)
         role = res.scalar_one_or_none()
         return role
+
+    async def _get_image_by_id(self, id: str) -> ImageModel | None:
+        query = select(ImageModel).where(ImageModel.id == id)  # noqa
+        res = await self.session.execute(query)
+        image = res.scalar_one_or_none()
+        return image
+
+
+class SQLImageRepository(IImageRepository):
+    model = ImageModel
+
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    async def create(self, id: str, file_url: str) -> bool:
+        image = ImageModel(id=id, file_url=file_url)
+        self.session.add(image)
+        await self.session.flush()
+        return True
+
+    async def get(self, id: str) -> str | None:
+        image = await self.session.get(self.model, id)
+        return image.file_url if image else None

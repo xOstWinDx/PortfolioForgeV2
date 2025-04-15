a5c53e51f1895c45a4133c02c9efaddbe60c6275
Alexey <Starobogatov.a@yandex.ru>
2025-04-15 06:44:17 +0300

    add exceptions handlers and pretty Docs openapi.

--- auth/main.py
+++ auth/main.py
@@ -1,37 +1,30 @@
 import logging
 from contextlib import asynccontextmanager
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Any
 
 import aio_pika
 from fastapi import FastAPI, HTTPException
+from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 from starlette import status
 from starlette.requests import Request
+from starlette.responses import HTMLResponse
 
 from src.config import settings
-from src.domain.exceptions import ConflictError, UnauthorizedError
+from src.domain.exceptions import ConflictError, AuthenticationError, AuthorizationError
 from src.infrastructure.exceptions import S3ClientException
+from src.presentation.http.docs.openapi import custom_openapi
 from src.presentation.http.router import router
 
 logging.basicConfig(level=logging.INFO)
 
 
-# ────────────────
-# TODO [12.04.2025 | High]
-# Assigned to: stark
-# Description: Реализовать
-# Steps:
-#   - Генерацию JWT
-#   - Аутентификацию Пользователя
-#   - Переиздание токенов
-#   - BLACKLIST токенов *и возможно WHITELIST*
-# ────────────────
 @asynccontextmanager
 async def life_span(app: FastAPI) -> AsyncGenerator[None, None]:
-    connection = await aio_pika.connect_robust(settings.RABBITMQ_URL)
-    app.state.connection = connection
+    connection = await aio_pika.connect_robust(settings.RABBITMQ_URL, timeout=10)
+    app.state.connection = connection  # noqa
     redis = aioredis.from_url(settings.REDIS_URL)
     backend = RedisBackend(redis)
     await backend.redis.ping()
@@ -44,7 +37,37 @@ async def life_span(app: FastAPI) -> AsyncGenerator[None, None]:
     await connection.close()
 
 
-app = FastAPI(title="Auth Service", version="0.0.1", lifespan=life_span)
+app = FastAPI(
+    title="Auth Service",
+    version="0.0.1",
+    openapi_url="",
+    docs_url="",
+    redoc_url="",
+    summary="API для управления пользователями и авторизацией.",
+    description=(
+        "## Описание сервиса\n\n"
+        "Микросервис для управления пользователями и авторизацией. "
+        "Предоставляет endpoints для регистрации, входа, обновления токенов и работы с профилем.\n\n"
+        "### Ключевые особенности\n"
+        "- **Авторизация**: OAuth2 Bearer токены через заголовок `Authorization: Bearer <token>`.\n"
+        "- **Валидация**: Все данные проверяются через Pydantic.\n"
+        "- **Кэширование**: Некоторые endpoints оптимизированы с помощью кэша. \n\n"
+        "### Связаться\n"
+        "- Telegram: [API Developer](https://t.me/m/zvhobMxSNGQy)\n"
+        "- Email: [Starobogatov.alexey@gmail.com](mailto:Starobogatov.alexey@gmail.com)"
+    ),
+    openapi_tags=[
+        {
+            "name": "Authentication",
+            "description": "Endpoints для работы с авторизацией.",
+        },
+        {
+            "name": "Users",
+            "description": "Endpoints для работы с пользователями.",
+        },
+    ],
+    lifespan=life_span,
+)
 
 app.include_router(router)
 
@@ -54,14 +77,43 @@ async def conflict_handler(request: Request, exc: ConflictError) -> None:
     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(exc))
 
 
-@app.exception_handler(UnauthorizedError)
-async def unauthorized_handler(request: Request, exc: UnauthorizedError) -> None:
+@app.exception_handler(AuthenticationError)
+async def unauthorized_handler(request: Request, exc: AuthenticationError) -> None:
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(exc))
 
 
 @app.exception_handler(S3ClientException)
 async def s3_client_exception_handler(request: Request, exc: S3ClientException) -> None:
-    raise HTTPException(
+    HTTPException(
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         detail="Failed to upload file",
     )
+
+
+@app.exception_handler(AuthorizationError)
+async def forbidden_handler(request: Request, exc: AuthorizationError) -> None:
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(exc))
+
+
+@app.get("/openapi.json", include_in_schema=False)
+async def openapi_json() -> dict[str, Any]:
+    return custom_openapi(app)  # type: ignore
+
+
+@app.get("/docs", response_class=HTMLResponse, include_in_schema=False)
+async def get_documentation() -> HTMLResponse:
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",
+        title="Auth Service API Documentation",
+        swagger_favicon_url="https://42812a87-8640-4d3e-a250-8550c4a8ce16.selstorage.ru/doc.logo.png",
+    )
+    # return get_redoc_html(
+    #     openapi_url="/openapi.json",
+    #     title="Auth Service API Documentation",
+    #     redoc_favicon_url="https://42812a87-8640-4d3e-a250-8550c4a8ce16.selstorage.ru/doc.logo.png"
+    # )
+
+
+@app.get("/health", status_code=200, include_in_schema=False)
+async def health() -> dict[str, str]:
+    return {"status": "ok"}

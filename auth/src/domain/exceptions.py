d031f38d16a66066d86c341486834a1a23f5a13c
Alexey <Starobogatov.a@yandex.ru>
2025-04-15 06:55:15 +0300

    new exceptions

--- auth/src/domain/exceptions.py
+++ auth/src/domain/exceptions.py
@@ -7,8 +7,26 @@ class ConflictError(Exception):
         return self.message
 
 
-class UnauthorizedError(Exception):
-    def __init__(self, message: str = "Unauthorized") -> None:
+class AuthenticationError(Exception):
+    def __init__(self, message: str = "Authentication error") -> None:
+        super().__init__(message)
+        self.message = message
+
+    def __repr__(self) -> str:
+        return self.message
+
+
+class AuthorizationError(Exception):
+    def __init__(self, message: str = "Authorization error") -> None:
+        super().__init__(message)
+        self.message = message
+
+    def __repr__(self) -> str:
+        return self.message
+
+
+class ValidationError(Exception):
+    def __init__(self, message: str = "Validation error") -> None:
         super().__init__(message)
         self.message = message
 
